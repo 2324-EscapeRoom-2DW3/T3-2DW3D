694275606b48f2c0ecc84fc0844db3a837ce1ee4
Marko Gros <markogrostx@gmail.com>
2023-12-14 09:22:44 +0100

    PUSHEATE TIO

---
+++ Erronka2/resources/components/Listas08.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'ListasOption',
+    components: {},
+    data() {
+        return {
+            personas: []
+        }
+    },
+    methods: {
+        cargarPersonas() {
+            axios.get('https://randomuser.me/api/?results=5')
+                .then(resp => {
+                    console.log(resp);
+                    this.personas = resp.data.results;
+                })
+        }
+    },
+    mounted() {
+        this.cargarPersonas();
+    }
+
+}
+
+</script>
+
+<template>
+    <h1 class="text-2xl font-bold mb-4">Listas Axios</h1>
+
+    <main class="flex flex-wrap justify-center">
+        <img v-for="persona in personas" :src="persona.picture.large" :alt="persona.name.first"
+            class="w-48 h-48 object-cover m-2">
+    </main>
+</template>
+
+<style></style>
---
+++ Erronka2/resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+import { createApp } from 'vue'
+import App from '../App.vue'
+   import '../css/app.css'
+
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+createApp(App).mount('#app')
