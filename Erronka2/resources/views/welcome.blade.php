a8b6be483de34992ed07e71e7fde649db3b3ee41
Mforza <markogrostx@gmail.com>
2023-12-18 11:59:58 +0100

    Plantilla fix

--- Erronka2/resources/views/welcome.blade.php
+++ Erronka2/resources/views/welcome.blade.php
@@ -1,9 +1,13 @@
-@extends('layouts.plantilla')
+@extends('layout.plantilla')
 
 
 @section('title', 'Index')
 
 @section('content')
 
+<body class="antialiased">
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
 
+    </div>
+</body>
 @endsection
