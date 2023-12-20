f3824da6cf4a9858679987f334407f0b9364f151
Mforza <markogrostx@gmail.com>
2023-12-20 11:43:50 +0100

    Index php + various fixes

--- Erronka2/resources/js/app.js
+++ Erronka2/resources/js/app.js
@@ -11,4 +11,9 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-createApp(App).mount('#app')
+const headerApp = createApp(App);
+headerApp.mount('#header-app');
+
+// Create a Vue instance for the footer
+const footerApp = createApp(App);
+footerApp.mount('#footer-app');
--- Erronka2/tailwind.config.js
+++ Erronka2/tailwind.config.js
@@ -1,29 +1,60 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import tailwindTypewriter from 'tailwind-typewriter';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views//.blade.php',
-        './index.html', './src/**/.{vue,js,ts,jsx,tsx}',
-
-        "./resources/**/*.blade.php",
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+  ],
 
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      backgroundPosition: {
+        bottom: 'bottom',
+        'bottom-4': 'center bottom 1rem',
+        center: 'center',
+        left: 'left',
+        'left-bottom': 'left bottom',
+        'left-top': 'left top',
+        right: 'right',
+        'right-bottom': 'right bottom',
+        'right-top': 'right top',
+        'right-4': 'center right 1rem',
+        top: 'top',
+        'top-4': 'center top 1rem',
+      },
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [forms, tailwindTypewriter({
+    wordsets: {
+      titulo: {
+        words: ['QUIMM'],
+        delay: 1,
+        caretColor: 'transparent',
+        repeat: 0,
+        eraseSpeed: 0,
+      },
+      titulo2: {
+        words: ['ICA'],
+        delay: 3,
+        caretColor: 'lime',
+        repeat: 0,
+        eraseSpeed: 0,
+        caretWidth: '5px',
+
+      },
+    },
+  })],
 };
-
-
