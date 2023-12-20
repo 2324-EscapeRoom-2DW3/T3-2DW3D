a5adaa8e7781667828d4320a28ca372da2a70777
Ibai <ibaigm04@gmail.com>
2023-12-20 13:21:05 +0100

    Slider vue

---
+++ Erronka2/resources/components/Login-register.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="overlay-container container" :class="{ 'right-panel-active': isRightPanelActive }" ref="container">
+      <div class="overlay">
+        <div class="overlay-panel overlay-left">
+          <h1>Welcome Back!</h1>
+          <p>To keep connected with us please login with your personal info</p>
+          <button class="ghost" id="signIn" @click="signIn">Sign In</button>
+        </div>
+        <div class="overlay-panel overlay-right">
+          <h1>Hello, Friend!</h1>
+          <p>Enter your personal details and start journey with us</p>
+          <button class="ghost" id="signUp" @click="signUp">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    data() {
+      return {
+        isRightPanelActive: false
+      };
+    },
+    methods: {
+      signUp() {
+        this.isRightPanelActive = true;
+      },
+      signIn() {
+        this.isRightPanelActive = false;
+      }
+    }
+  }
+  </script>
+
+  <style scoped>
+  /* Your other CSS rules */
+
+  /* LOGIN */
+  /* Rest of your CSS styles */
+  </style>
--- Erronka2/resources/views/layouts/guest.blade.php
+++ Erronka2/resources/views/layouts/guest.blade.php
@@ -15,16 +15,19 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased ">
-        <div class="min-h-screen shadow-4xl flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-center"  style="background-image: url('storage/images/fondo.png'); background-position: -700px -375px;">
+        <div class=" pl-20 pr-20  min-h-screen shadow-4xl flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-center"  style="background-image: url('storage/images/fondo.png'); background-position: -700px -375px;">
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-green-900  shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full mt-6 px-6 py-4 bg-green-900  shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
+
+
         </div>
+
     </body>
 </html>
