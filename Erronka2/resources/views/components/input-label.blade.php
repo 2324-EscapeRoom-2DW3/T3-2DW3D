6d4c7bf01a92999a9776ebf7a8a4306b3730a61b
Ibai <ibaigm04@gmail.com>
2023-12-22 09:35:50 +0100

    bold

--- Erronka2/resources/views/components/input-label.blade.php
+++ Erronka2/resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
 <label {{ $attributes->merge(['class' => ' italic font-inter text-xl opacity-90 block font-medium text-sm dark:text-green-300']) }}>
-    <span class="font-inter text-green-600 shining-light">{{ $value ?? $slot }}</span>
+    <span class="font-bold font-inter text-green-600 shining-light">{{ $value ?? $slot }}</span>
 </label>
