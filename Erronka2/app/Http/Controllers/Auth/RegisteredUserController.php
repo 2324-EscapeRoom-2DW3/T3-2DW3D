77dc1e3a4efa98909732a5001c26262cf8a4e335
Parlitakeloke <trollert33@gmail.com>
2024-01-15 11:53:30 +0100

    login y register

--- Erronka2/app/Http/Controllers/Auth/RegisteredUserController.php
+++ Erronka2/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,13 +34,51 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'imagen' => ['required'],
+
+
+            
         ]);
+
+        // $file = $request->file('imagen');
+        // $file->move(base_path('\storage\app\public\images'), $file->getClientOriginalName());
+        // $nombre = $request->name;
+        // $file_name = $_FILES['imagen']['name'];
+        // $file_path = $_FILES['imagen']['tmp_name'];
+        // $target_dir = base_path('\storage\app\public\images');
+        // $target_file = $target_dir . basename($file_name);
+
+        // move_uploaded_file($file_path, $target_file);
+
+        // Obtener el nombre del archivo subido
+$file_name = $_FILES['imagen']['name'];
+
+// Obtener el nombre del usuario (reemplaza 'nombre_de_usuario' con la lógica real para obtener el nombre del usuario)
+$user_name = $request->name;
+
+// Obtener la extensión del archivo
+$file_extension = pathinfo($file_name, PATHINFO_EXTENSION);
+
+// Crear un nuevo nombre para el archivo basado en el nombre del usuario
+$new_file_name = $user_name . '.' . $file_extension;
+
+// Ruta del directorio donde deseas guardar el archivo
+$upload_directory = base_path('\storage\app\public\images');
+
+// Ruta completa del nuevo archivo
+$new_file_path = $upload_directory . $new_file_name;
+//mover
+move_uploaded_file($_FILES['imagen']['tmp_name'], $new_file_path);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'rol' => '0',
+            'imagen' => $new_file_name,
+
+
+            
         ]);
 
         event(new Registered($user));
