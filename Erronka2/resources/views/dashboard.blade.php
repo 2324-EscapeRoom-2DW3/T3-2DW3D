3df2f80eb1c97ee997442d656fffcc96d18b51e4
Ibai <ibaigm04@gmail.com>
2024-01-17 09:07:10 +0100

    a

--- Erronka2/resources/views/dashboard.blade.php
+++ Erronka2/resources/views/dashboard.blade.php
@@ -8,6 +8,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    @php
+                    $photoFilename = Auth::user()->foto;
+                    $photoFilename = $photoFilename;
+                    $imageUrl = url('storage/images/' . $photoFilename);
+                    @endphp
+
+                    <img src='"{{ $imageUrl }}" . "images"' alt="User Photo">
                 </div>
             </div>
         </div>
