dccaad34596c6967577960062587941659c4caa0
Ibai <ibaigm04@gmail.com>
2024-01-17 08:50:44 +0100

    a

--- Erronka2/resources/views/partials/header.blade.php
+++ Erronka2/resources/views/partials/header.blade.php
@@ -55,7 +55,7 @@
             class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center z-50" >
             <a class="mr-10 hover-effect">Hasiera</a>
             <a class="mr-10 hover-effect">Jolasak</a>
-            <a href="{{ route('guri') }}" class="mr-10 hover-effect">Guri Buruz</a>
+            <a href="{{ route('guri') }}" class="mr-10 hover-effect">FAQ</a>
 
         </nav>
       {{--   <nav class="inline-flex items-center py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 z-50">
