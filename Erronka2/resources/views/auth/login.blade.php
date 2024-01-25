462453347c808d4ada20f1720e1056995a7dcee3
ByMarko99 <marko.grosal@elorrieta-errekamari.com>
2024-01-25 19:36:33 +0100

    Revert "login-register-responsive"
    This reverts commit 64f2056043e7d7469c97b9abc3ea524630dea295.

--- Erronka2/resources/views/auth/login.blade.php
+++ Erronka2/resources/views/auth/login.blade.php
@@ -105,6 +105,70 @@
 
                     </div>
 
+                <div class="mt-4 contenedo">
+
+                        <!-- <x-input-label for="foto" value="Argazkia" />
+                        <input type="file" name="foto" class="block mt-1 w-full"> -->
+                        <div class="input-div">
+                            <input class="inputs" name="imagen" type="file">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
+                                <polyline points="16 16 12 12 8 16"></polyline>
+                                <line y2="21" x2="12" y1="12" x1="12"></line>
+                                <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                                <polyline points="16 16 12 12 8 16"></polyline>
+                            </svg>
+                        </div>
+
+                    </div>
+
+                <div class="mt-4 contenedo">
+
+                        <!-- <x-input-label for="foto" value="Argazkia" />
+                        <input type="file" name="foto" class="block mt-1 w-full"> -->
+                        <div class="input-div">
+                            <input class="inputs" name="imagen" type="file">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
+                                <polyline points="16 16 12 12 8 16"></polyline>
+                                <line y2="21" x2="12" y1="12" x1="12"></line>
+                                <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                                <polyline points="16 16 12 12 8 16"></polyline>
+                            </svg>
+                        </div>
+
+                    </div>
+
+                <div class="mt-4 contenedo">
+
+                        <!-- <x-input-label for="foto" value="Argazkia" />
+                        <input type="file" name="foto" class="block mt-1 w-full"> -->
+                        <div class="input-div">
+                            <input class="inputs" name="imagen" type="file">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
+                                <polyline points="16 16 12 12 8 16"></polyline>
+                                <line y2="21" x2="12" y1="12" x1="12"></line>
+                                <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                                <polyline points="16 16 12 12 8 16"></polyline>
+                            </svg>
+                        </div>
+
+                    </div>
+
+                <div class="mt-4 contenedo">
+
+                        <!-- <x-input-label for="foto" value="Argazkia" />
+                        <input type="file" name="foto" class="block mt-1 w-full"> -->
+                        <div class="input-div">
+                            <input class="inputs" name="imagen" type="file">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
+                                <polyline points="16 16 12 12 8 16"></polyline>
+                                <line y2="21" x2="12" y1="12" x1="12"></line>
+                                <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                                <polyline points="16 16 12 12 8 16"></polyline>
+                            </svg>
+                        </div>
+
+                    </div>
+
                 <div class="flex items-center justify-end mt-4">
                     <a class="underline text-sm text-white hover:text-green-300 dark:hover:text-green-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                         href="{{ route('login') }}">
