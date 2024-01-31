5d97ff9d1d09ea53fd80698046b250c4a2c94831
Parlitakeloke <trollert33@gmail.com>
2024-01-31 12:22:14 +0100

    boton logout

--- Erronka2/resources/views/partials/header.blade.php
+++ Erronka2/resources/views/partials/header.blade.php
@@ -67,10 +67,12 @@
       @if(!isset($valor))
       <a href="{{ url('/perfilIndex') }}" class="mr-5 underline-spacing"><?php echo Auth::user()->name; ?>
       </a>
+      <div id="botonLogout">
       <form action="{{ route('logout') }}" method="POST">
         @csrf
       <input type="submit" value="LogOut">
       </form>
+      </div>
       @else
       <a href="{{ url('/profile') }}" class="mr-5 underline-spacing"><?php echo Auth::user()->name; ?>
       </a>
